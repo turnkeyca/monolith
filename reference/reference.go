38491215c036bde9a507566ad9adcc8b62b7baad
rileytoddherman <rileyherman@tuta.io>
2021-05-14 18:15:02 -0600

    add some initial files

---
+++ reference/reference.go
@@ -0,0 +1,30 @@
+package user
+
+import (
+	"log"
+
+	"github.com/turnkeyca/monolith/db"
+)
+
+type Handler struct {
+	logger *log.Logger
+	db     *db.Database
+}
+
+type KeyId struct{}
+type KeyBody struct{}
+
+func NewHandler(logger *log.Logger, db *db.Database) *Handler {
+	return &Handler{
+		logger: logger,
+		db:     db,
+	}
+}
+
+type GenericError struct {
+	Message string `json:"message"`
+}
+
+type ValidationError struct {
+	Messages []string `json:"messages"`
+}
