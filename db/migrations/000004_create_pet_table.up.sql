a94c7186c678dcebb76aa5c225871b5ea36fd7d2
brookeham <bham.313@gmail.com>
2021-07-11 10:24:11 -0600

    removal of 'not null' and column changes to match adalo

--- db/migrations/000004_create_pet_table.up.sql
+++ db/migrations/000004_create_pet_table.up.sql
@@ -1,7 +1,8 @@
 create table pet (
     id uuid primary key,
-    user_id uuid not null, 
-    breed varchar(64),
-    weight numeric(10, 2),
+    user_id uuid, 
+    pet_type varchar(64),
+	breed varchar(64),
+    size_type varchar(64),
     constraint FK_user_id foreign key (user_id) references users(id)
 );
