49380d0848df6b17c0a998c9ab09e9988f2a9afa
brookeham <bham.313@gmail.com>
2021-07-11 09:50:30 -0600

    removal of 'not null'

--- db/migrations/000003_create_reference_table.up.sql
+++ db/migrations/000003_create_reference_table.up.sql
@@ -1,7 +1,7 @@
 create table reference (
     id uuid primary key,
-    user_id uuid not null, 
-    full_name varchar(64) not null,
+    user_id uuid, 
+    full_name varchar(64),
     email varchar(64),
 	phone_number varchar(64),
     relationship varchar(64),
