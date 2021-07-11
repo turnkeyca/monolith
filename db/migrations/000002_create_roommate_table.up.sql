2435ee5e06e8563f8860ed47bad933f69949d14b
brookeham <bham.313@gmail.com>
2021-07-11 09:41:24 -0600

    removal of 'not null'

--- db/migrations/000002_create_roommate_table.up.sql
+++ db/migrations/000002_create_roommate_table.up.sql
@@ -1,7 +1,7 @@
 create table roommate (
     id uuid primary key,
-    user_id UUID not null, 
-    full_name varchar(64) not null,
+    user_id uuid, 
+    full_name varchar(64),
     email varchar(64),
     additional_details varchar(256),
     constraint FK_user_id foreign key (user_id) references users(id)
